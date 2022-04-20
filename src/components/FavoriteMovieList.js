756b89eac58a4207a91ce567b7c8b0296150e21c
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-04-19 20:03:21 -0400

    Able to add and delete movies from list

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';   
 
 
 const FavoriteMovieList = (props) => {
@@ -22,4 +23,4 @@ const FavoriteMovieList = (props) => {
 }
 
 
-export default FavoriteMovieList;
+export default connect()(FavoriteMovieList);
